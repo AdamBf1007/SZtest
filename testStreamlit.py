f5f053301d6e1e41d87b28045514a158b384e6ab
AdamBf1007 <79919962+AdamBf1007@users.noreply.github.com>
2024-10-20 15:42:18 +0200

    Update testStreamlit.py

--- testStreamlit.py
+++ testStreamlit.py
@@ -1,37 +1,46 @@
 import streamlit as st
+import pandas as pd
 
-# Exemple de liste à afficher
-data = [
-    {"Frame": 0, "Texte": "Cell contre Buu"},
-    {"Frame": 0, "Texte": "Prends ça ! Kamehameha enragé !"},
-    {"Frame": 0, "Texte": "Lève-toi, Son Goku !"},
-    {"Frame": 0, "Texte": "Fais vite, Jalousie !"},
-    {"Frame": 0, "Texte": "La véritable puissance de Freezer"},
-    {"Frame": 180, "Texte": "Buu se déchaîne"},
-    {"Frame": 180, "Texte": "La puissance latente de Saiyen"},
-    {"Frame": 180, "Texte": "Protège jalousie de Freezer de Planète Namek !"},
-    {"Frame": 180, "Texte": "Le fils de Son Goku"},
-    {"Frame": 180, "Texte": "La détermination de Namek"},
-    {"Frame": 210, "Texte": "La jalousie (ilin"}
-]
+# Charger les données depuis un fichier Excel
+file_path = 'fichier_modifié.xlsx'
+data = pd.read_excel(file_path)
 
-# Titre de l'application
-st.title("Recherche de Textes")
-
-# Champ de recherche
-search_text = st.text_input("Rechercher un texte")
-
-# Filtrer les options basées sur la recherche
-options = [item['Texte'] for item in data if search_text.lower() in item['Texte'].lower()]
-
-# Liste déroulante
-selected_item = st.selectbox("Sélectionnez un texte", options)
-
-# Afficher le texte correspondant à l'élément sélectionné
-if selected_item:
-    for item in data:
-        if item['Texte'] == selected_item:
-            st.markdown(f"**Frame :** {item['Frame']} | **Texte :** {item['Texte']}")
-            break
+# Vérifiez que les colonnes "Frame", "Text" et "Pourcentage" existent
+if not all(col in data.columns for col in ['Frame', 'Text', 'Pourcentage']):
+    st.error("Le fichier Excel doit contenir les colonnes 'Frame', 'Text' et 'Pourcentage'.")
 else:
-    st.write("Aucun élément sélectionné.")
+    # Titre de l'application
+    st.title("Recherche de textes en CUSTOM BATTLE (SparkingZero)")
+
+    # Champ de recherche
+    search_text = st.text_input("Rechercher un texte")
+
+    # Filtrer les options basées sur la recherche
+    options = [row['Text'] for index, row in data.iterrows() if search_text.lower() in row['Text'].lower()]
+
+    # Liste déroulante
+    selected_item = st.selectbox("Sélectionnez un texte", options)
+
+    # Afficher le texte correspondant à l'élément sélectionné
+    if selected_item:
+        selected_row = data[data['Text'] == selected_item]
+        if not selected_row.empty:
+            frame_value = selected_row['Frame'].values[0]
+            percentage_value = selected_row['Pourcentage'].values[0]
+             # Afficher le texte avec une taille de police plus grande
+            st.markdown(f"""
+            <p style='font-size:24px;'><strong style='color:green;'>Texte :</strong> {selected_item}</p>
+            <p style='font-size:24px;'><strong style='color:green;'>Pourcentage (+/-) :</strong> {percentage_value}</p>
+            """, unsafe_allow_html=True)
+            st.markdown(f"\n\n")
+            st.markdown(f"\n\n")
+            st.markdown(f"\n\n")
+            # Texte rouge en mode indication
+            st.markdown("<span style='color:yellow; font-weight: bold;'>GUIDE D'UTILISATION :\n\n-Il ne faut utiliser aucun filtre\n\n-Il faut garder l'indication \"Tout\" en dessous du terme Légendes comme montré sur l'image ci-dessous.\n\n-Une fois que vous aurez recherché votre terme voulu, la liste contiendra toutes les occurrences de ce terme parmi les 5000+ légendes disponibles.\n\n-Le POURCENTAGE associé indique l'endroit jusqu'auquel il vous faudra scroller pour arriver à votre terme souhaité PAR RAPPORT A L'ECHELLE de la barre latérale de scroll.\n\n-En d'autres termes, si le pourcentage lié à votre texte indique 79%, cela veut dire qu'il faut scroller à peu près jusqu'aux trois quarts de la barre (en passant par en bas c'est plus facile donc).\n\nNB : Il y a encore certaines imprécisions mais ça devrait grandement vous aider à créer vos CUSTOM BATTLE sans passer 24h à chercher une phrase/un mot.</span>", unsafe_allow_html=True)
+            st.markdown("<span style='color:red; font-weight: bold;'>NB : Il y a encore certaines imprécisions mais ça devrait grandement vous aider à créer vos CUSTOM BATTLE sans passer 24h à chercher une phrase/un mot.</span>", unsafe_allow_html=True)
+            # Ajouter une image
+            image_path = 'guide.png'  # Spécifiez le chemin de votre image
+            st.image(image_path, caption="Image GUIDE", use_column_width=True)
+
+    else:
+        st.write("Aucun élément sélectionné.")
